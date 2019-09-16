7844445e57553582f8f49efdbef39c7d18f66989
Miles Mickelson <milesmickelson87@gmail.com>
2019-09-16 11:11:08 -0700

    adds redux store and real reducers

--- src/js/components/ExpenseEntries/ExpenseEntries.jsx
+++ src/js/components/ExpenseEntries/ExpenseEntries.jsx
@@ -1,4 +1,4 @@
-import React from 'react;'
+import React from 'react';
 
 export default class ExpenseEntries extends React.Component {
   constructor(props) {
--- src/js/components/IncomeEntries/IncomeEntries.jsx
+++ src/js/components/IncomeEntries/IncomeEntries.jsx
@@ -1,4 +1,4 @@
-import React from 'react;'
+import React from 'react';
 
 export default class IncomeEntries extends React.Component {
   constructor(props) {
